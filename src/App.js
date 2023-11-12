112f537caf43f9800ef724342770e41003c483d6
YohannesNi <yohannes.addis03@gmail.com>
2023-11-12 11:46:11 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import AlertSection from "./components/Main/AlertSection";
+import FirstSection from "./components/Main/FirstSection";
+import SecondSection from "./components/Main/SecondSection";
+import ThirdSection from "./components/Main/ThirdSection";
+import FourthSection from "./components/Main/FourthSection";
+import FifthSection from "./components/Main/FifthSection";
+import SixthSection from "./components/Main/SixthSection";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <AlertSection />
+      <FirstSection />
+      <SecondSection />
+      <ThirdSection />
+      <FourthSection />
+      <FifthSection />
+      <SixthSection />
+      <Footer />
     </div>
   );
 }
